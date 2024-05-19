c750bb0cd3da2b2c2405bd42d68c67e549e5445f
Maad-Ali <maad.alshaik@gmail.com>
2024-05-19 14:07:01 +0300

    GSessionID

--- src/test/java/base_urls/BaseURL.java
+++ src/test/java/base_urls/BaseURL.java
@@ -4,18 +4,23 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
-import static utilties.Authentication.generateToken;
+import static utilties.GSessionID.SessionID;
 
-public class BaseURL {
+
+public class BaseURL  {
 
     public static RequestSpecification spec;
 
     static {
-        spec = new RequestSpecBuilder()
-                .setBaseUri("https://a3m-qa-gm3.quaspareparts.com/auth/api")
-                .addHeader("Authorization","Bearer "+generateToken())
-                .setContentType(ContentType.JSON)
-                .build();
+            spec = new RequestSpecBuilder()
+                    .setBaseUri("https://qa-gm3.quaspareparts.com/a3m/auth/api")
+    //                .addHeader("Authorization","Bearer "+generateToken())
+                    .addHeader("Cookie","GSESSIONID="+SessionID())
+                    .setContentType(ContentType.JSON)
+                    .build();
+
     }
+
+
 
 }
