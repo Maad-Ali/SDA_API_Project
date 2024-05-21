fc8805415f394e5ec7413cbb26afd58fefcf56f1
HarbiAsma <asma_alharbi@outlook.com>
2024-05-21 13:10:13 +0300

    Asma API

--- src/test/java/Requests/Roles/GetRolesByApp.java
+++ src/test/java/Requests/Roles/GetRolesByApp.java
@@ -3,6 +3,7 @@ package Requests.Roles;
 import base_urls.BaseURL;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
+
 import static io.restassured.RestAssured.given;
 
 public class GetRolesByApp extends BaseURL {
