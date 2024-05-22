c1034207e64eeaea120d3635119db99ecc406d57
leen-1422 <leen8288@yahoo.com>
2024-05-22 09:35:37 +0300

    fix

--- src/test/java/pojos/Leen/OrganizationByAPP.java
+++ src/test/java/pojos/Leen/OrganizationByAPP.java
@@ -1,7 +1,10 @@
 package pojos.Leen;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class OrganizationByAPP{
 	private Integer number_of_users;
 	private Integer founder_id;
